9b3fd936a8756cfb82be44b499c9f226a771b0e1
kwinkunks <matt@agilegeoscience.com>
2016-02-19 16:28:21 -0400

    carrying on, nearly ready for merge

--- welly/curve.py
+++ welly/curve.py
@@ -37,6 +37,30 @@ class Curve(np.ndarray):
         self.mnemonic = getattr(obj, 'mnemonic', None)
         self.units = getattr(obj, 'units', None)
 
+    def _repr_html_(self):
+        """
+        Jupyter Notebook magic repr function.
+        """
+        attribs = self.__dict__.copy()
+        row1 = '<tr><th style="text-align:center;" colspan="2">{} [{{}}]</th></tr>'
+        rows = row1.format(attribs.pop('mnemonic'))
+        rows = rows.format(attribs.pop('units', '&ndash;'))
+        row2 = '<tr><td style="text-align:center;" colspan="2">{:.4f} : {:.4f} : {:.4f}</td></tr>'
+        rows += row2.format(attribs.pop('start'), self.stop, attribs.pop('step'))
+        s = '<tr><td><strong>{k}</strong></td><td>{v}</td></tr>'
+        for k, v in attribs.items():
+            rows += s.format(k=k, v=v)
+        s = '<tr><th style="border-top: 2px solid #000;">Depth</th><th style="border-top: 2px solid #000;">Value</th></tr>'
+        rows += s.format(self.start, self[0])
+        s = '<tr><td>{:.4f}</td><td>{:.4f}</td></tr>'
+        for depth, value in zip(self.basis[:3], self[:3]):
+            rows += s.format(depth, value)
+        rows += '<tr><td>⋮</td><td>⋮</td></tr>'
+        for depth, value in zip(self.basis[-3:], self[-3:]):
+            rows += s.format(depth, value)
+        html = '<table>{}</table>'.format(rows)
+        return html
+
     @property
     def stop(self):
         return self.start + self.shape[0] * self.step
@@ -91,6 +115,43 @@ class Curve(np.ndarray):
         ax.grid()
         return
 
+    def new_basis(self, start=None, stop=None, step=None):
+        """
+        Reset the start, stop, and/or step.
+        """
+        params = self.__dict__.copy()
+        old_basis = self.basis
+
+        if start is not None:
+            # This will crop the top of the log.
+            # Get the first surviving index.
+            new_start_index = self._read_at(start, index=True) + 1
+            new_start = float(start)
+        else:
+            new_start_index = 0
+            new_start = self.start
+
+        if stop is not None:
+            adj = 0 if step is None else 1
+            new_stop_index = self._read_at(stop, index=True) + adj
+            new_stop = float(stop)
+        else:
+            new_stop_index = None
+            new_stop = self.stop
+
+        data = np.copy(self)[new_start_index:new_stop_index]
+        params['start'] = new_start
+
+        if step is not None:
+            new_adj_stop = new_stop + step/100  # To guarantee inclusion.
+            new_basis = np.arange(new_start, new_adj_stop, step)
+            basis = old_basis[new_start_index:new_stop_index]
+            data = np.interp(new_basis, basis, data)
+            params['step'] = float(step)
+
+        return Curve(data, params)
+
+    # DEPRECATE WHEN WE KNOW WHAT NEW_BASIS DOES
     def segment(self, d):
         """
         Returns a segment of the log between the depths specified.
@@ -101,11 +162,11 @@ class Curve(np.ndarray):
         Returns:
             Curve. The new curve segment.
         """
-        top = self._read_at(d[0], index=True)
+        top = self._read_at(d[0], index=True) + 1  # b/c returns index before
         base = self._read_at(d[1], index=True)
 
         data = self[top:base]
-        params = self.__dict__.copy()  # copy attributes from main curve
+        params = self.__dict__.copy()
         params['start'] = d[0]
         return Curve(data, params)
 
@@ -199,24 +260,17 @@ class Curve(np.ndarray):
 
         # Find the tops of the 'zones'.
         tops, vals = utils.find_edges(data)
+        np.append(tops, None)
+        np.append(vals, None)
 
         if values is None:
             # Transform each segment in turn, then deal with the last segment.
             for top, base in zip(tops[:-1], tops[1:]):
                 data[top:base] = f(np.copy(self[top:base]))
-            data[base:] = f(np.copy(self[base:]))
+            #data[base:] = f(np.copy(self[base:]))
         else:
             for top, base, val in zip(tops[:-1], tops[1:], vals[:-1]):
                 data[top:base] = values[int(val)]
-            data[base:] = values[int(vals[-1])]
+            #data[base:] = values[int(vals[-1])]
 
         return Curve(data, params)
-
-    def mean(self):
-        """
-        Could have all sorts of helpful transforms etc.
-        """
-        try:
-            return np.mean(np.copy(self))
-        except:
-            raise CurveError("You can't do that.")
