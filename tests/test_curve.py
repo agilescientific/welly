97a9879f02b7a23df8843e8fe8d7449d42fed352
kwinkunks <matt@agilegeoscience.com>
2016-03-23 17:07:04 -0300

    more tests, tidied up

---
+++ tests/test_curve.py
@@ -0,0 +1,73 @@
+# -*- coding: utf 8 -*-
+"""
+Define a suite a tests for the Curve module.
+"""
+import numpy as np
+
+from welly import Well
+from welly import Curve
+
+FNAME = 'tests/P-129_out.LAS'
+
+
+def test_curve():
+
+    well = Well.from_las(FNAME)
+    gr = well.data['GR']
+
+    # Basics
+    assert gr.ndim == 1
+    assert gr.size == 12718
+    assert gr.basis.size == 12718
+
+    # Check HTML repr.
+    html = gr._repr_html_()
+    base = """<table><tr><th style="text-align:center;" colspan="2">GR [gAPI]</th></tr><tr><td style="text-align:center;" colspan="2">1.0668 : 1939.2900 : 0.1524</td></tr><tr><td><strong>"""
+    co = """<tr><td><strong>service_company</strong></td><td>Schlumberger</td></tr>"""
+    null = """<tr><td><strong>null</strong></td><td>-999.25</td></tr>"""
+    data = """<tr><th style="border-top: 2px solid #000;">Depth</th><th style="border-top: 2px solid #000;">Value</th></tr><tr><td>1.0668</td><td>46.6987</td></tr><tr><td>1.2192</td><td>46.6987</td></tr><tr><td>1.3716</td><td>46.6987</td></tr><tr><td>⋮</td><td>⋮</td></tr><tr><td>1938.8328</td><td>92.2462</td></tr><tr><td>1938.9852</td><td>92.2462</td></tr><tr><td>1939.1376</td><td>92.2462</td></tr></table>"""
+    assert base in html
+    assert co in html
+    assert null in html
+    assert data in html
+
+
+def test_basis():
+    well = Well.from_las(FNAME)
+    gr = well.data['GR']
+
+    x = gr.to_basis(start=100, stop=200, step=1)
+    assert x.size == 101
+    assert x[0] - 66.6059 < 0.001
+
+    y = gr.to_basis_like(x)
+    assert y.size == 101
+    assert y[0] - 66.6059 < 0.001
+
+
+def test_read():
+    well = Well.from_las(FNAME)
+    gr = well.data['GR']
+
+    assert gr.read_at(1000) - 109.414177 < 0.001
+
+    actual = gr.read_at([500, 1000, 1500])
+    desired = np.array([91.29946709, 109.4141766, 64.55931458])
+    np.testing.assert_allclose(actual, desired)
+
+
+def test_block():
+    well = Well.from_las(FNAME)
+    gr = well.data['GR']
+
+    b = gr.block(cutoffs=[50, 100])
+    assert b.size == 12718
+    assert b.basis.size == 12718
+    assert b.max() == 2
+
+    b = gr.block()
+    assert b.mean() - 0.46839 < 0.001
+
+    b = gr.block(cutoffs=[50, 100], values=[12, 24, 36])
+    assert b.max() == 36
+    assert b.mean() - 25.077528 < 0.001
---
+++ tests/test_location.py
@@ -0,0 +1,67 @@
+# -*- coding: utf 8 -*-
+"""
+Define a suite a tests for the Location module.
+"""
+import re
+
+import numpy as np
+
+from welly import Well
+from welly import utils
+
+FNAME = 'tests/P-129_out.LAS'
+DNAME = 'tests/P-129_deviation_survey.csv'
+
+
+def test_deviation():
+
+    well = Well.from_las(FNAME)
+    dev = np.loadtxt(DNAME, delimiter=',', skiprows=1)
+    well.location.add_deviation(dev)
+    assert well.location.position.shape == (46, 3)
+    assert well.location.md2tvd(1000) - 987.03517 < 0.001
+    assert well.location.tvd2md(987.03517) - 1000 < 0.001
+
+
+def test_well_remap():
+
+    def transform_ll(text):
+
+        def callback(match):
+            d = match.group(1).strip()
+            m = match.group(2).strip()
+            s = match.group(3).strip()
+            c = match.group(4).strip()
+            if c.lower() in ('w', 's') and d[0] != '-':
+                d = '-' + d
+            return ' '.join([d, m, s])
+
+        regex = r".+?([-0-9]+?).? ?([0-9]+?).? ?([\.0-9]+?).? +?([NESW])"
+        pattern = re.compile(regex, re.I)
+        text = pattern.sub(callback, text)
+        return utils.dms2dd([float(i) for i in text.split()])
+
+    remap = {
+        'LATI': 'LOC',  # Use LOC for the parameter LATI.
+        'LONG': 'UWI',  # Use UWI for the parameter LONG.
+        'SECT': None,   # Use nothing for the parameter SECT.
+        'RANG': None,   # Use nothing for the parameter RANG.
+        'TOWN': None,   # Use nothing for the parameter TOWN.
+    }
+
+    funcs = {
+        'LATI': transform_ll,  # Pass LATI through this function before load.
+        'LONG': transform_ll,  # Pass LONG through it too.
+        'UWI': lambda x: "No name, oh no!"
+    }
+
+    well = Well.from_las(FNAME, remap=remap, funcs=funcs)
+
+    # Check some basics.
+    assert (well.location.latitude - 45.20951027) < 0.001
+    assert well.uwi == 'No name, oh no!'
+
+    # Check CRS
+    well.location.crs_from_epsg(4269)
+    assert well.location.crs.data['no_defs']
+    assert well.location.crs.to_string() == '+init=epsg:4269 +no_defs'
