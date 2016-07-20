03652c484a8d1ac857f64e5d26ede449a5535a6e
kwinkunks <matt@agilegeoscience.com>
2016-07-20 14:05:53 -0300

    error check was a bug

--- tests/test_project.py
+++ tests/test_project.py
@@ -12,5 +12,16 @@ def test_project():
     project = Project.from_las('tests/1.las')
     assert len(project) == 1
 
-    project += Well.from_las('tests/2.las')
+    w = Well.from_las('tests/2.las')
+    project += w
+    assert w in p
     assert len(project) == 2
+
+    project += project
+
+    assert project.uwis[0] == '1'
+
+    s = "<table><tr><th>UWI</th><th>Data</th><th>Curves</th></tr><tr><td>300A524400060300</td>"
+    assert s in p._repr_html_()
+
+    assert p[1] == w
