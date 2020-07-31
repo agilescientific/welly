b8451d002b31d71bbd64240f07942b02290f890e
dcslagel <dcs@mailworks.org>
2020-07-31 11:55:09 -0600

    Convert empty string param values to None read_las
    - At well.read_lasio() convert empty string param values without a unit to None
    - Include tests for this change in tests/test_location.py

--- welly/well.py
+++ welly/well.py
@@ -324,6 +324,10 @@ class Well(object):
 
         las = lasio.read(fname, encoding=encoding)
 
+        for key, value in las.params.items():
+            if las.params[key].unit == "" and las.params[key].value in ["", "None"]:
+                las.params[key].value = None
+
         # Pass to other constructor.
         return cls.from_lasio(las,
                               remap=remap,
@@ -623,7 +627,6 @@ class Well(object):
              **kwargs):
         """
         Plot multiple tracks.
-
         Args:
             legend (striplog.legend): A legend instance.
             tracks (list): A list of strings and/or lists of strings. The
