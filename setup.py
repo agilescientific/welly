cc0769690c8a4bba60b6aa61b0bbbc0b16bee411
Matt Hall <matt@agilegeoscience.com>
2021-03-02 12:35:01 -0400

    versions

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ CLASSIFIERS = ['Development Status :: 3 - Alpha',
                'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7',
                'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
                ]
 
 setup(name='welly',
