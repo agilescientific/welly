ffeb08b15c34552fa2c66f4cc53332120f792531
Zabamund <fracgeol@gmail.com>
2021-03-26 13:06:49 +0100

    Refactor compute_position_log
    In the process of moving welly to use wellpathpy to handle all
    deviation and position data, the API has not been changed in
    location.py, rather the compute_position_log function now uses
    wellpathpy to return the same objects as it did previously.
    
    There are no breaking changes but there are now additional methods
    available to welly to compute a position log.
    
    The same adjustments to TD, surface and azimuth_datum that were
    made previously have simply been included in the new function and
    remain in welly, rather than being added to wellpathpy.

--- welly/tools.py
+++ welly/tools.py
@@ -5,6 +5,7 @@ Some extra bits.
 :license: Apache 2.0
 """
 import numpy as np
+import wellpathpy as wp
 
 from . import utils
 from .curve import Curve
@@ -18,7 +19,73 @@ class RGBLog(object):
         pass
 
 
-def compute_position_log(deviation,
+def compute_position_log(deviation, td=None, method='mc', azimuth_datum=0):
+    """Use wellpathpy to return the required welly objects
+
+    Args:
+        deviation (ndarray): A deviation survey with rows like MD, INC, AZI
+        td (Number): The TD of the well, if not the end of the deviation
+            survey you're passing.
+        method (str):
+            'mc': minimum curvature
+            'aa': average angle
+            'bt': balanced tangential
+            'hi': high tangential
+            'lo': low tangential
+            'rc': radius of curvature
+
+        update_deviation: This function makes some adjustments to the dev-
+            iation survey, to account for the surface and TD. If you do not
+            want to change the stored deviation survey, set to False.
+        azimuth_datum (Number): The orientation of the azimuth datum,
+            relative to the y-axis.
+
+    Returns:
+        ndarray. A position log with rows like X-offset, Y-offset, Z-offset
+    """
+
+    # Adjust to TD.
+    if td is not None:
+        last_row = np.copy(deviation[-1, :])
+        last_row[0] = td
+        deviation = np.vstack([deviation, last_row])
+
+    # Adjust to surface if necessary.
+    if deviation[0, 0] > 0:
+        deviation = np.vstack([np.array([0, 0, 0]), deviation])
+
+    # Adjust to azimuth_datum.
+    deviation += [0, 0, azimuth_datum]
+
+    # Make wellpathpy objects
+    md, inc, azi = np.split(deviation, 3, 1)
+    dev = wp.deviation(md, inc, azi)
+
+    if method == 'mc':
+        pos = dev.minimum_curvature()
+    elif method == 'aa':
+        pos = dev.tan_method()
+    elif method == 'bt':
+        pos = dev.tan_method(choice='bal')
+    elif method == 'hi':
+        pos = dev.tan_method(choice='high')
+    elif method == 'lo':
+        pos = dev.tan_method(choice='low')
+    elif method == 'rc':
+        pos = dev.radius_curvature()
+    else:
+        msg = 'uknown choice {}, must be one of {}'
+        methods = 'mc aa bt hi lo rc'.split()
+        raise ValueError(msg.format(method, ' '.join(methods)))
+
+    # Convert to welly formats
+    deviation = np.hstack((dev.md, dev.inc, dev.azi))
+    position = np.hstack((pos.easting.reshape(-1, 1), pos.northing.reshape(-1, 1), pos.depth.reshape(-1, 1)))
+    dogleg = dev.minimum_curvature().dls.reshape(-1, 1)
+
+    return deviation, position, dogleg
+
+def compute_position_log_welly(deviation,
                          td=None,
                          method='mc',
                          azimuth_datum=0
