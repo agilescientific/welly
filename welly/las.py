4e720eb730aaefef7e8a0bd64ec5cd9842ba0a79
patrick.reinhard <patrick.reinhard@shell.com>
2021-10-08 16:58:19 +0200

    change path to file_ref, pep8 line length, file from url, get las version

--- welly/las.py
+++ welly/las.py
@@ -1,24 +1,29 @@
+import warnings
+from io import StringIO
+from urllib import error, request
+
 import lasio
 import numpy as np
 import pandas as pd
 from lasio import HeaderItem, CurveItem, SectionItems
-import warnings
 
 from welly import utils
 from welly.fields import curve_sections, other_sections, header_sections
 from welly.utils import get_columns_decimal_formatter
 
-# the lasio header item dictionary contain the fields from which we will parse the LASFile header metadata
+# the lasio header item dictionary contains the fields from which we will parse
+# the LASFile header metadata
 header_item = HeaderItem().__dict__
 
-# the lasio curve item dictionary contain the fields from which we will parse the LASFile curve header metadata
+# the lasio curve item dictionary contains the fields from which we will parse
+# the LASFile curve header metadata
 curve_item = CurveItem().__dict__
 
-# set pandas precision higher than the default of 5 so that it will not automatically round off the curve data
+# set pandas precision higher to not automatically round the curve data
 pd.set_option('precision', 10)
 
 
-def from_las(path, **kwargs):
+def from_las(file_ref, **kwargs):
     """
     Read LAS file with lasio and parse every LAS dataset to 2 pd.DataFrames:
         1. curve data
@@ -30,7 +35,8 @@ def from_las(path, **kwargs):
     https://lasio.readthedocs.io/en/latest/header-section.html#tutorial
 
     Args:
-        path (str): Path to LAS file
+        file_ref (file-like object, str): either a filename, an open file
+            object, or a string containing the contents of a LAS file.
         **kwargs: The additional keyword arguments are propagated to the lasio reader so you can use when reading in a
                   LAS file. Find the routines of the different keyword possibilities look here:
                     * :func:`lasio.reader.open_with_codecs` - manage issues relate to character encodings
@@ -86,27 +92,39 @@ def from_las(path, **kwargs):
         })
     """
     # read las file
-    las = lasio.read(path, **kwargs)
+    las = lasio.read(file_ref, **kwargs)
 
-    # get las format version
-    version = float(las.version[0].value)
-
-    if version == 2.0 or version == 1.2:
-        datasets = from_las2(las)
-    else:
-        try:
-            datasets = from_las2(las)
-            warnings.warn(f"Warning, LAS version {version} not yet supported. "
-                          "Attempting to use LAS<=2.0 parsing logic for LAS3.0.")
-        except Exception:
-            raise NotImplementedError(f"LAS version {version} not yet supported")
+    datasets = from_lasio(las)
 
     return datasets
 
 
-def from_las2(las):
+def from_lasio(las):
     """
-    Parse lasio.LASFile (LAS version 1.2 & 2.0) to 2 pd.DataFrames that form 1 datasets entry as a tuple.
+
+    """
+    # get las format version
+    version = float(las.version[0].value)
+
+    if version == 2.0 or version == 1.2:
+        datasets = from_las_2_or_older(las)
+    else:
+        try:
+            datasets = from_las_2_or_older(las)
+            warnings.warn(f"Warning, LAS version {version} not yet supported. "
+                          "Attempting to use LAS 1.2 and 2.0 parsing logic "
+                          "for LAS 3.0.")
+        except Exception:
+            raise NotImplementedError(
+                f"LAS version {version} not yet supported")
+
+    return datasets
+
+
+def from_las_2_or_older(las):
+    """
+    Parse lasio.LASFile (LAS version 1.2 & 2.0) to 2 pd.DataFrames that form 1
+    datasets entry as a tuple.
 
     For LAS versions 1.2 & 2.0, 1 LAS file translates to 'Curves' 1 data set.
 
@@ -160,7 +178,8 @@ def from_las2(las):
         # section composed of SectionItem instance(s)
         if section in header_sections:
             # construct df
-            df_section = pd.DataFrame([i.__dict__ for i in las.sections[section]])
+            df_section = pd.DataFrame(
+                [i.__dict__ for i in las.sections[section]])
             # append section name as separate column
             df_section['section'] = section
             # concat metadata to header df
@@ -170,18 +189,22 @@ def from_las2(las):
             if section in curve_sections:
                 if len(las.sections[section]) > 0:
                     # construct df
-                    df_section = pd.DataFrame([i.__dict__ for i in las.sections[section]])
+                    df_section = pd.DataFrame(
+                        [i.__dict__ for i in las.sections[section]])
                     # parse curve data to separate df
-                    df_section = pd.DataFrame(np.matrix(df_section.data.tolist()).transpose(),
-                                              columns=df_section.mnemonic.values)
+                    df_section = pd.DataFrame(
+                        np.matrix(df_section.data.tolist()).transpose(),
+                        columns=df_section.mnemonic.values)
                     # concat to curve data df
                     data = pd.concat([data, df_section])
                     # all curves are parsed as strings if there is one string present somewhere
                     for column in data.columns:
                         if data[column].dtype == 'O':
                             # replacement of string null values with np.nan
-                            data[column] = data[column].replace(str(las.well["NULL"].value), np.nan)
-                            data[column] = data[column].replace(str(las.well["NULL"].value) + '.0', np.nan)
+                            data[column] = data[column].replace(
+                                str(las.well["NULL"].value), np.nan)
+                            data[column] = data[column].replace(
+                                str(las.well["NULL"].value) + '.0', np.nan)
                             try:
                                 # attempt to convert numeric curves to float type
                                 data[column] = data[column].astype(np.float64)
@@ -200,7 +223,9 @@ def from_las2(las):
             header = pd.concat([header, df_section])
 
         else:
-            warnings.warn('This section was not recognized and therefore not parsed: "{}"'.format(section))
+            warnings.warn(
+                'This section was not recognized and therefore not parsed: "{}"'.format(
+                    section))
 
     header.drop(['data'], axis=1, inplace=True)
 
@@ -235,20 +260,33 @@ def to_las(path, datasets, **kwargs):
                 df_section = header[header.section == section_name]
 
                 if section_name == 'Version':
-                    if len(df_section[df_section.original_mnemonic == 'VERS']) > 0:
-                        las.version.VERS = df_section[df_section.original_mnemonic == 'VERS']['value'].values[0]
-                    if len(df_section[df_section.original_mnemonic == 'WRAP']) > 0:
-                        las.version.WRAP = df_section[df_section.original_mnemonic == 'WRAP']['value'].values[0]
-                    if len(df_section[df_section.original_mnemonic == 'DLM']) > 0:
-                        las.version.DLM = df_section[df_section.original_mnemonic == 'DLM']['value'].values[0]
+                    if len(df_section[
+                               df_section.original_mnemonic == 'VERS']) > 0:
+                        las.version.VERS = \
+                            df_section[df_section.original_mnemonic == 'VERS'][
+                                'value'].values[0]
+                    if len(df_section[
+                               df_section.original_mnemonic == 'WRAP']) > 0:
+                        las.version.WRAP = \
+                            df_section[df_section.original_mnemonic == 'WRAP'][
+                                'value'].values[0]
+                    if len(df_section[
+                               df_section.original_mnemonic == 'DLM']) > 0:
+                        las.version.DLM = \
+                            df_section[df_section.original_mnemonic == 'DLM'][
+                                'value'].values[0]
 
                 elif section_name == 'Well':
                     las.sections["Well"] = SectionItems(
-                        [HeaderItem(r.original_mnemonic, r.unit, r.value, r.descr) for i, r in df_section.iterrows()])
+                        [HeaderItem(r.original_mnemonic, r.unit, r.value,
+                                    r.descr) for i, r in
+                         df_section.iterrows()])
 
                 elif section_name == 'Parameter':
                     las.sections["Parameter"] = SectionItems(
-                        [HeaderItem(r.original_mnemonic, r.unit, r.value, r.descr) for i, r in df_section.iterrows()])
+                        [HeaderItem(r.original_mnemonic, r.unit, r.value,
+                                    r.descr) for i, r in
+                         df_section.iterrows()])
 
                 elif section_name == 'Curves':
                     for i, header_row in df_section.iterrows():
@@ -264,21 +302,65 @@ def to_las(path, datasets, **kwargs):
                     las.sections["Other"] = df_section['descr'][0]
 
                 else:
-                    warnings.warn('Section was not recognized and not parsed: "{}"'.format(section_name))
+                    warnings.warn(
+                        'Section was not recognized and not parsed: "{}"'.format(
+                            section_name))
         else:
             raise NotImplementedError('LAS 3.0 is not yet supported.')
 
         # get the numeric null value representation if it exists in the header (e.g. -9999)
         try:
-            null_value = header[header.original_mnemonic == 'NULL'].value.iloc[0]
+            null_value = header[header.original_mnemonic == 'NULL'].value.iloc[
+                0]
         except IndexError:
             null_value = None
 
-        # las.write defaults to %.5 decimal points where we would like to retain the number of decimals
-        # therefore we first construct a column formatter based on  the max number of decimal points found in each curve
-        if not 'column_fmt' in kwargs:
-            kwargs['column_fmt'] = get_columns_decimal_formatter(data=las.data, null_value=null_value)
+        # las.write defaults to %.5 decimal points where we like to retain
+        # the nr of decimals therefore we first construct a column formatter
+        # based on the max number of decimal points found in each curve.
+        if 'column_fmt' not in kwargs:
+            kwargs['column_fmt'] = get_columns_decimal_formatter(
+                data=las.data, null_value=null_value)
 
         # write file to disk
         with open(path, mode='w') as f:
             las.write(f, **kwargs)
+
+
+def file_from_url(url):
+    """
+    Retrieve a file from an HTTPS URL and return it as an in-memory stream
+    for text.
+
+    Args:
+        url (str): URL to file.
+
+    Returns:
+        text_file (StringIO): an in-memory stream for text.
+    """
+    try:
+        text_file = StringIO(request.urlopen(url).read().decode())
+    except error.HTTPError as e:
+        raise Exception('Could not retrieve url: ', e)
+
+    return text_file
+
+
+def get_las_version(las):
+    """
+    Get the LAS file format version from an in-memory lasio.LAFile object.
+
+    There are 3 possible versions (https://www.cwls.org/products/):
+        - LAS 1.2
+        - LAS 2.0
+        - LAS 3.0
+
+    Args:
+        las (lasio.LASFile): An in-memory lasio.LASFile object
+
+    Returns:
+        version (float): LAS format version
+    """
+    version = float(las.version[0].value)
+
+    return version
