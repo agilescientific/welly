0205c43c29ffa21ffb294d4472fc99e7be8d032c
Matt Hall <matt@agilescientific.com>
2022-02-18 13:31:38 -0400

    better docs, new Project intro

--- docs/post_process_html.py
+++ docs/post_process_html.py
@@ -8,13 +8,17 @@ def simplify_credits(html):
     """
     Replace the credit part of the HTML footer. Return the new text.
     """
-    s = r"Created using <a href=\"https://www\.sphinx.+?Furo theme</a>."
-    pattern = re.compile(s, flags=re.DOTALL)
+    s = r'<a class="muted-link" href="https://pradyunsg\.me">@pradyunsg</a>\'s'
+    pattern = re.compile(s)
+    html = pattern.sub(r'', html)
 
-    new_s = '<a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>'
-    new_s += ' | Created using Sphinx & Furo'
+    s = r'Copyright &#169; 2022, Agile Scientific'
+    pattern = re.compile(s)
+    new_s = '&#169; 2022, Agile Scientific | <a href="https://creativecommons.org/licenses/by/4.0/">CC BY</a>'
+    html = pattern.sub(new_s, html)
 
-    return pattern.sub(new_s, html)
+    return html
+
 
 def main(path):
     """
